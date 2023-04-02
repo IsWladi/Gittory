e718b73cc4d2c18727461263fddef3a0d767e5d0
Wladimir <wl.urzua@duocuc.cl>
2023-04-01 22:49:54 -0300

    first commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.gittory = require("gittory")
+
+return M
---
+++ lua/gittory.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+-- Función para buscar el directorio raíz del repositorio de Git
+function M.find_git_root()
+  local path = vim.loop.cwd()
+  local i = 0
+  while path:match('.:\\') and i < 10 do
+    if vim.fn.isdirectory(path .. '/.git') == 1 then
+      return path
+    end
+    path = vim.fn.fnamemodify(path, ':h')
+    i = i + 1
+  end
+end
+
+-- Función para buscar en todo el directorio de trabajo del repositorio de Git con Telescope
+function M.search_git_root()
+  local is_git = M.isGitRepository()
+  if is_git then
+    local git_root = M.find_git_root()
+    if git_root then
+      require('telescope.builtin').find_files({ cwd = git_root })
+    else
+      print('El repositorio tiene más de 10 directorios superiores respecto al directorio de trabajo actual')
+    end
+  else
+    print('No es un repositorio de Git')
+  end
+end
+
+function M.isGitRepository()
+  local isGit = os.execute('git status > NUL 2>&1') -- 0 si es un repositorio de Git, 1 si no lo es, el msg de retorno se guarda en NUL(se elimina)
+  if type(isGit) == 'number' then --si es lua 5.2 o superior el tipo de retorno es number
+    isGit = (isGit == 0)
+  end
+  if isGit then
+    return true
+  else
+    return false
+  end
+end
+
+
+
+return M

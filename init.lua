7dcd4ed1355f8b9814236a0e1b2b70e481b0315c
wladimir_pc2 <wl.urzua@duocuc.cl>
2023-06-05 22:32:30 -0400

    testing with BufReadPost

--- init.lua
+++ init.lua
@@ -3,6 +3,6 @@ local M = {}
 M.gittory = require("gittory")
 
 -- Llamar a la función set_git_root() al iniciar Neovim
-vim.cmd([[autocmd VimEnter * lua require('gittory').gittory.set_git_root()]])
+vim.cmd([[autocmd BufReadPost * lua require('gittory').gittory.set_git_root()]])
 
 return M

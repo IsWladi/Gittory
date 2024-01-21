e571e9a9837c59d107fcdc9ea0370060ae60d4d1
wladi <wladimir2000@protonmail.com>
2024-01-21 20:14:34 -0300

    refactor: isGitRepository and get_git_root functions
    The isGitRepository and get_git_root are much more simple and better
    I basically adapted a code recipe from the telescope wiki:
    https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#find-files-from-project-git-root-with-fallback

--- lua/main.lua
+++ lua/main.lua
@@ -5,24 +5,13 @@ M.backUpPath = nil
 
 -- Function to check if the current directory is a Git repository
 function M.isGitRepository()
-  local windows = string.lower(vim.loop.os_uname().sysname)
-  local isWindows = string.match(windows, "windows")
-  local isGit = nil
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    return vim.v.shell_error == 0 -- return true if the current directory is a Git repository
+end
 
-  if not isWindows then
-    isGit = os.execute('git status > /dev/null 2>&1') -- 0 if it is a Git repository, 1 if it is not, the return message is saved in /dev/null (it is deleted)
-  else
-    isGit = os.execute('git status > NUL 2>&1') -- For Unix systems, not tested right now
-  end
-
-  if type(isGit) == 'number' then -- if it is lua 5.2 or higher the return type is number
-    isGit = (isGit == 0)
-  end
-  if isGit then
-    return true
-  else
-    return false
-  end
+function M.get_git_root()
+  local dot_git_path = vim.fn.finddir(".git", ".;")
+  return vim.fn.fnamemodify(dot_git_path, ":h")
 end
 
 -- Function to print messages with or without the rcarriga/nvim-notify plugin.
@@ -34,47 +23,48 @@ function M.printMessage(isNotifyInstalled, message, type, options)
   end
 end
 
--- Function to set the root directory of the Git repository for being used at startup of Neovim
+-- Function to set the root directory of the Git repository
 function M.set_git_root(settings)
-  settings.notify = settings.notify or "not"
+  -- When the user calls GittoryDesactivate, the settings.backUpPath is set to the current working directory where the user opened Neovim.
+  -- if it is different from "not", then the user wants to desactivate gittory
   settings.backUpPath = settings.backUpPath or "not"
-  local path = vim.loop.cwd()
-  M.backUpPath = path -- for use in the nvim_create_user_command for desactivate gittory
-  local home = vim.loop.os_homedir()
-  local i = 0
+
+  -- this part is for protect the variable M.backUpPath to be overwritten and don´t lose the backup path
+  if M.isInitialized ~= true then
+    local path = vim.loop.cwd()
+    M.backUpPath = path -- for use in the nvim_create_user_command for desactivate gittory
+    M.isInitialized = true
+  end
   local is_git = M.isGitRepository()
+
   if is_git then
-    while path ~= home do
-      if vim.fn.isdirectory(path .. '/.git') == 1 then
-        local shortPath = nil
-        if settings.backUpPath ~= "not" then
-          vim.api.nvim_set_current_dir(settings.backUpPath)
-          if settings.notify == "yes" then
-            shortPath = settings.backUpPath:match("[^/\\]+$")
-            vim.defer_fn(function()
-              M.printMessage(ok, 'Actual folder: /'..shortPath..'/' , 'success', { title = 'Gittory', render = "compact" })
-            end, 1500) --  (1.5 seconds)
-          end
-        else
-          vim.api.nvim_set_current_dir(path) -- Change the current directory to the root of the Git repository
-          if settings.notify == "yes" then
-            shortPath = path:match("[^/\\]+$")
-            vim.defer_fn(function()
-              M.printMessage(ok, 'Folder´s project: /'..shortPath..'/' , 'success', { title = 'Gittory', render = "compact" })
-            end, 1500) --  (1.5 seconds)
-          end
-        end
-        return
+    shortPath = nil -- variable where the name of the folder´s project will be stored
+
+    if settings.backUpPath ~= "not" then -- the user want to desactivate gittory with the command GittoryDesactivate
+      vim.api.nvim_set_current_dir(settings.backUpPath)
+      M.isInitialized = false
+      if settings.notify == "yes" then
+        shortPath = settings.backUpPath:match("[^/\\]+$")
+        vim.defer_fn(function()
+          M.printMessage(ok, 'Actual folder: /'..shortPath..'/' , 'success', { title = 'Gittory', render = "compact" })
+        end, 1500) --  (1.5 seconds)
       end
-      path = vim.fn.fnamemodify(path, ':h')
-      i = i + 1
+    else -- the user want to activate gittory with the command GittoryInit or at startup
+      M.git_root_path = M.get_git_root()
+      vim.api.nvim_set_current_dir(M.git_root_path) -- Change the current directory to the root of the Git repository
+
+      if settings.notify == "yes" then  -- notify the root of the actual git repository
+        shortPath = M.git_root_path:match("[^/\\]+$")
+        if shortPath == "." then -- for fix the bug when the root of the git repository is the same as the cwd
+          shortPath = vim.loop.cwd():match("[^/\\]+$") -- get the name of the folder´s project and avoid "/./" in the message
+        end
+        -- print the success message
+        vim.defer_fn(function()
+          M.printMessage(ok, 'Folder´s project: /'..shortPath..'/' , 'success', { title = 'Gittory', render = "compact" })
+        end, 1500) --  (1.5 seconds)
+      end
     end
 
-    if settings.notify == "yes" then
-        vim.defer_fn(function()
-          M.printMessage(ok, 'No /.git/ found. The search is maximum up to /home/', 'error', { title = 'Gittory', render = "compact" })
-        end, 1500) --  (1.5 seconds)
-    end
   elseif settings.notify == "yes" then
     vim.defer_fn(function()
       M.printMessage(ok, 'This is not a Git repository. The actual path is being used.', 'info', { title = 'Gittory', render = "compact" })
@@ -86,6 +76,7 @@ function M.setup(options)
   options = options or {}
   options.atStartUp = options.atStartUp or "not"
   options.notify = options.notify or "not"
+  M.isInitialized = false -- for protect the variable M.backUpPath to be overwritten
   vim.api.nvim_create_user_command("GittoryInit",
     function ()
         M.set_git_root( {notify = options.notify} )
@@ -105,4 +96,3 @@ function M.setup(options)
 end
 
 return M
-

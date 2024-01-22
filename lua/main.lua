0f365d6b4c47a7077e25218ba2f3a7ea17d4b56c
wladi <wladimir2000@protonmail.com>
2024-01-22 01:22:51 -0300

    refactor: delete duplicated code

--- lua/main.lua
+++ lua/main.lua
@@ -23,6 +23,21 @@ function M.printMessage(isNotifyInstalled, message, type, options)
   end
 end
 
+-- Function to print various purposes messages
+function M.printResponseMessage(response, options, type)
+  if options == "show path" then
+    local shortPath = M.git_root_path:match("[^/\\]+$")
+    if shortPath == "." then -- for fix the bug when the root of the git repository is the same as the cwd
+      shortPath = vim.loop.cwd():match("[^/\\]+$") -- get the name of the folder´s project and avoid "/./" in the message
+    end
+    response = response .. ': /'..shortPath..'/'
+  end
+  -- print the message
+  vim.defer_fn(function()
+    M.printMessage(ok, response, type, { title = 'Gittory', render = "compact" })
+  end, 1000) --  (1 seconds)
+end
+
 -- Function to set the root directory of the Git repository
 function M.set_git_root(settings)
   -- When the user calls GittoryDesactivate, the settings.backUpPath is set to the current working directory where the user opened Neovim.
@@ -38,37 +53,23 @@ function M.set_git_root(settings)
   local is_git = M.isGitRepository()
 
   if is_git then
-    shortPath = nil -- variable where the name of the folder´s project will be stored
-
     if settings.backUpPath ~= "not" then -- the user want to desactivate gittory with the command GittoryDesactivate
       vim.api.nvim_set_current_dir(settings.backUpPath)
       M.isInitialized = false
       if settings.notify == "yes" then
-        shortPath = settings.backUpPath:match("[^/\\]+$")
-        vim.defer_fn(function()
-          M.printMessage(ok, 'Actual folder: /'..shortPath..'/' , 'success', { title = 'Gittory', render = "compact" })
-        end, 1500) --  (1.5 seconds)
+        M.printResponseMessage("Actual Folder", "show path", "succes") -- notify the path where the user opened Neovim
       end
     else -- the user want to activate gittory with the command GittoryInit or at startup
       M.git_root_path = M.get_git_root()
       vim.api.nvim_set_current_dir(M.git_root_path) -- Change the current directory to the root of the Git repository
 
       if settings.notify == "yes" then  -- notify the root of the actual git repository
-        shortPath = M.git_root_path:match("[^/\\]+$")
-        if shortPath == "." then -- for fix the bug when the root of the git repository is the same as the cwd
-          shortPath = vim.loop.cwd():match("[^/\\]+$") -- get the name of the folder´s project and avoid "/./" in the message
-        end
-        -- print the success message
-        vim.defer_fn(function()
-          M.printMessage(ok, 'Folder´s project: /'..shortPath..'/' , 'success', { title = 'Gittory', render = "compact" })
-        end, 1500) --  (1.5 seconds)
+        M.printResponseMessage("Folder´s project", "show path", "success") -- notify the root of the actual git repository
       end
     end
 
   elseif settings.notify == "yes" then
-    vim.defer_fn(function()
-      M.printMessage(ok, 'This is not a Git repository. The actual path is being used.', 'info', { title = 'Gittory', render = "compact" })
-    end, 1500) --  (1.5 seconds)
+    M.printResponseMessage("This is not a Git repository. The actual path is being used.", "normal", "info")
   end
 end
 

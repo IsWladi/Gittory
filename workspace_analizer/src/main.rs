048dafa331154a5e8919e962c3b596f474799fd0
IsWladi <wladimir2000@protonmail.com>
2024-09-29 19:03:02 -0300

    feat(rust): get recognized applications function

--- workspace_analizer/src/main.rs
+++ workspace_analizer/src/main.rs
@@ -1,4 +1,5 @@
-use clap::Parser;
+use clap::{builder::Str, Parser};
+use std::collections::HashMap;
 use std::path::Path;
 use walkdir::{DirEntry, WalkDir};
 
@@ -18,10 +19,17 @@ struct Args {
     ignore: Vec<String>, // not implemented yet
 }
 
+#[derive(Debug)]
 struct ApplicationType {
     name: String,
     files: Vec<String>,
     directories: Vec<String>,
+}
+
+#[derive(Debug)]
+struct RecognizedApplication {
+    name: String,
+    path: String,
 }
 
 fn main() {
@@ -31,18 +39,23 @@ fn main() {
 
     let application_types: Vec<ApplicationType> = vec![ApplicationType {
         name: String::from("Rust"),
-        files: vec![
-            String::from("Cargo.toml"),
-            String::from("Cargo.lock"),
-            String::from("src/*.rs"),
-        ],
+        files: vec![String::from("Cargo.toml"), String::from("Cargo.lock")],
         directories: vec![String::from("target"), String::from("src")],
     }];
 
-    get_dir(&args.path, &args.depth, &application_types);
+    dbg!(&application_types);
+
+    let recognized_applications: Vec<RecognizedApplication> =
+        get_recognized_applications(&args.path, &args.depth, &application_types);
+
+    dbg!(recognized_applications);
 }
 
-fn get_dir(path: &String, depth: &usize, application_types: &Vec<ApplicationType>) {
+fn get_recognized_applications(
+    path: &String,
+    depth: &usize,
+    application_types: &Vec<ApplicationType>,
+) -> Vec<RecognizedApplication> {
     fn is_hidden(entry: &DirEntry) -> bool {
         entry
             .file_name()
@@ -53,30 +66,104 @@ fn get_dir(path: &String, depth: &usize, application_types: &Vec<ApplicationType
 
     let walker = WalkDir::new(path)
         .min_depth(1)
-        .max_depth(depth.clone())
+        .max_depth(*depth)
         .into_iter();
+
+    let mut directories_map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut recognized_applications: Vec<RecognizedApplication> = Vec::new();
+
+    let mut previous_parent_dir = String::from("");
+
     for entry in walker.filter_entry(|e| !is_hidden(e)) {
         if let Ok(entry) = entry {
-            //TODO: group by parent directory
-            println!(
-                "Parent dir: {}",
-                get_parent_dir(&entry.path().display().to_string())
-            );
+            let parent_dir = get_parent_dir(entry.path().display().to_string());
+            let resource_name = get_resource_name(entry.path().display().to_string());
 
-            if &entry.path().is_dir() == &true {
-                println!("{} is a directory\n", &entry.path().display());
-            } else {
-                println!("{} is a file\n", &entry.path().display());
+            if previous_parent_dir.is_empty() {
+                previous_parent_dir = parent_dir.clone();
+            }
+
+            if previous_parent_dir != parent_dir {
+                if let Some(folder_list) = directories_map.get(&previous_parent_dir) {
+                    let name = recognize_application(folder_list.clone(), &application_types);
+                    if let Some(name) = name {
+                        recognized_applications.push(RecognizedApplication {
+                            name: name,
+                            path: previous_parent_dir.clone(),
+                        });
+                    }
+                }
+                previous_parent_dir = parent_dir.clone();
+            }
+
+            directories_map
+                .entry(parent_dir)
+                .or_insert_with(Vec::new)
+                .push(resource_name);
+        }
+    }
+
+    // Reconocer la aplicación para el último directorio
+    if let Some(folder_list) = directories_map.get(&previous_parent_dir) {
+        let name = recognize_application(folder_list.clone(), &application_types);
+        if let Some(name) = name {
+            recognized_applications.push(RecognizedApplication {
+                name: name,
+                path: previous_parent_dir.clone(),
+            });
+        }
+    }
+
+    recognized_applications
+}
+
+fn recognize_application(
+    folder_list: Vec<String>,
+    application_types: &Vec<ApplicationType>,
+) -> Option<String> {
+    let mut recognized_application: Option<String> = None;
+
+    for application_type in application_types {
+        let mut recognized = true;
+
+        for file in &application_type.files {
+            if !folder_list.contains(file) {
+                recognized = false;
+                break;
             }
         }
+
+        if recognized {
+            for directory in &application_type.directories {
+                if !folder_list.contains(directory) {
+                    recognized = false;
+                    break;
+                }
+            }
+        }
+
+        if recognized {
+            recognized_application = Some(application_type.name.clone());
+            break;
+        }
+    }
+
+    match recognized_application {
+        Some(name) => Some(name),
+        None => None,
     }
 }
 
-fn get_parent_dir(path: &str) -> &str {
-    let path = Path::new(path);
+fn get_parent_dir(path: String) -> String {
+    let path = Path::new(&path);
     if let Some(parent) = path.parent() {
-        parent.to_str().unwrap_or("")
+        parent.to_str().unwrap_or("").to_string()
     } else {
-        ""
+        "".to_string()
     }
+}
+
+fn get_resource_name(path: String) -> String {
+    let path = Path::new(&path);
+    path.file_name().unwrap().to_str().unwrap_or("").to_string()
 }
